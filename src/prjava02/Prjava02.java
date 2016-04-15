da08d82f195411ccd54d8c9c77e2f7b8f5aff8ff
zhji1516daw2 <zhji1516daw2@gmail.com>
2016-04-15 20:17:43 +0200

    Treballant amb ordres Git i branques v0.2

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -16,6 +16,7 @@ public class Prjava02 {
     public static void main(String[] args) {
         System.out.println("versió 0.1 del projecte prjava02"); 
         System.out.println("Creació d'una branca del projecte prjava02");
+        System.out.println("Afegint més codi a la branca00 del projecte prjava02");
         try {
             InetAddress adreça = InetAddress.getLocalHost();
             String hostname = adreça.getHostName();
