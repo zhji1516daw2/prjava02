ccc3e98187f5fe61b35e75c221d2c77f27a45027
zjiang1991iam <zjiang1991iam@gmail.com>
2016-04-22 17:09:00 +0200

    Creant un primer conflicte amb la branca01

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -25,6 +25,8 @@ public class Prjava02 {
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
             System.out.println("Versió  OS: " + System.getProperty("os.version"));
+            System.out.println("Conflicte típic: Codi diferent a una mateixa línia");
+
         }
         catch (IOException e) {
         }  
